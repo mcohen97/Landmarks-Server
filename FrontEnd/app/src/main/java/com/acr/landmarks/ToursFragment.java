6f4b578fa79c83939253666612ac0fe1ee7a4e1e
Luciano <lucho-rufo@hotmail.com>
2019-04-02 22:54:02 -0300

    Se agrega activity inicial con menú de pestañas y se pasa la lógica del mapa a un fragment.

---
+++ FrontEnd/app/src/main/java/com/acr/landmarks/ToursFragment.java
@@ -0,0 +1,20 @@
+package com.acr.landmarks;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ToursFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tours, container, false);
+    }
+}
