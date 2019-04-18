2adcd3f9c761ad892d03c560fddcdb25eabf1821
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-17 21:41:58 -0300

    se agregan mas pruebas para la clase landmark

--- BackEnd/ObligatorioISP/ObligatorioISP.BusinessLogic.Tests/LandmarkTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.BusinessLogic.Tests/LandmarkTest.cs
@@ -11,5 +11,23 @@ namespace ObligatorioISP.BusinessLogic.Tests
         {
             Landmark landmark = new Landmark("", 0.0, 0.0, "description", "iconPath");
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidLandmarkException))]
+        public void ShouldThrowExceptionWhenDescriptionIsNull() {
+            Landmark landmark = new Landmark("title", 0.0, 0.0, null, "iconPath");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidLandmarkException))]
+        public void ShouldThrowExceptionWhenPathDoesNotExist()
+        {
+            Landmark landmark = new Landmark("title", 0.0, 0.0, "description", "iconPath");
+        }
+
+        [TestMethod]
+        public void ShouldCreateLandmarkIfDataIsValid() {
+            Landmark landmark = new Landmark("", 0.0, 0.0, "description", "testImage.jpg");
+        }
     }
 }
