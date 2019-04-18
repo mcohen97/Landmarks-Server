286ff23470ee56625b529c7fa7ea1efc22f6b8cd
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-18 19:02:30 -0300

    se crea la clase referenciada en la prueba

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess/SqlServerLandmarksRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ObligatorioISP.DataAccess
+{
+    public class SqlServerLandmarksRepository
+    {
+        private string connectionString;
+        public SqlServerLandmarksRepository(string connString) {
+            connectionString = connString;
+        }
+    }
+}
