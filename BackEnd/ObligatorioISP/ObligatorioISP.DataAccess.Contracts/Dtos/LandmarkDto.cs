8f586d434ec6d68c925b5f6cec326b4005f075b9
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-19 17:28:29 -0300

    se agregan datos al dto de Landmark

--- BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Contracts/Dtos/LandmarkDto.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Contracts/Dtos/LandmarkDto.cs
@@ -6,5 +6,11 @@ namespace ObligatorioISP.DataAccess.Contracts.Dtos
 {
     public class LandmarkDto
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Description { get; set; }
+        public string ImageBase64 { get; set; }
     }
 }
