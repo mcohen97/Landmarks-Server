d059c892e39cf613c4f0229d6d5a01b955c93027
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-18 21:57:37 -0300

    se importa moq y se crea mock de repositorio de landmarks

--- BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/LandmarkControllerTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/LandmarkControllerTest.cs
@@ -1,13 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ObligatorioISP.DataAccess.Contracts;
 
 namespace ObligatorioISP.WebAPI.Tests
 {
     [TestClass]
     public class LandmarkControllerTest
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
+        private LandmarksController controller;
+        private Mock<ILandmarksRepository> landmarks;
+    
+        [TestInitialize]
+        public void SetUp() {
+            landmarks = new Mock<ILandmarksRepository>();
+            controller = new LandmarksController(landmarks);
         }
     }
 }
