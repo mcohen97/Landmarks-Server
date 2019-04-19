30104fa8b6aa04e0957a01b264d5a650cd93d777
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-19 17:28:15 -0300

    se agrega prueba para GET del controller

--- BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/LandmarkControllerTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/LandmarkControllerTest.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ObligatorioISP.DataAccess.Contracts;
@@ -25,11 +25,55 @@ namespace ObligatorioISP.WebAPI.Tests
         [TestMethod]
         public void ShouldReturn200OKWhenGET() {
 
+            double bottomLeftLat = -34.923844;
+            double bottomLeftLng = -56.170590;
+            double topRightLat = -34.908501;
+            double topRightLng = -56.155019;
+
+            IActionResult result = controller.Get(bottomLeftLat, bottomLeftLng, topRightLat, topRightLng);
+            OkResult ok = result as OkResult;
+            ICollection<LandmarkDto> landmarks = ok.Value as ICollection<LandmarkDto>;
+
         }
 
         private ICollection<LandmarkDto> GetFakeLandmarks()
         {
-            throw new NotImplementedException();
+            ICollection<LandmarkDto> sampleList = new List<LandmarkDto>() {
+                new LandmarkDto(){
+                    Id = 1,
+                    Title = "Landmark 1",
+                    Latitude = -34.912126,
+                    Longitude =-56.167282,
+                    Description = "Description 1",
+                    ImageBase64 = ""
+                },
+                new LandmarkDto(){
+                    Id = 2,
+                    Title = "Landmark 2",
+                    Latitude = -34.912900,
+                    Longitude =-56.162263,
+                    Description = "Description 2",
+                    ImageBase64 = ""
+                },
+                new LandmarkDto(){
+                    Id = 3,
+                    Title = "Landmark 3",
+                    Latitude = -34.914202,
+                    Longitude =-56.157930,
+                    Description = "Description 3",
+                    ImageBase64 = ""
+                },
+                new LandmarkDto(){
+                    Id = 4,
+                    Title = "Landmark 4",
+                    Latitude = -34.910866,
+                    Longitude =-56.183353,
+                    Description = "Description 4",
+                    ImageBase64 = ""
+                }
+
+            };
+            return sampleList;
         }
     }
 }
