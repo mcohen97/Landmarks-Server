9362675a7fddddaec8bc0bf261fc79f0f26c9590
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-18 22:07:26 -0300

    se hace setup del mock para programar una respuesta

--- BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/LandmarkControllerTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/LandmarkControllerTest.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ObligatorioISP.DataAccess.Contracts;
+using ObligatorioISP.DataAccess.Contracts.Dtos;
+using ObligatorioISP.WebAPI.Controllers;
 
 namespace ObligatorioISP.WebAPI.Tests
 {
@@ -13,7 +17,19 @@ namespace ObligatorioISP.WebAPI.Tests
         [TestInitialize]
         public void SetUp() {
             landmarks = new Mock<ILandmarksRepository>();
-            controller = new LandmarksController(landmarks);
+            landmarks.Setup(l => l.GetWithinCoordenates(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(GetFakeLandmarks());
+            controller = new LandmarksController(landmarks.Object);
+        }
+
+        [TestMethod]
+        public void ShouldReturn200OKWhenGET() {
+
+        }
+
+        private ICollection<LandmarkDto> GetFakeLandmarks()
+        {
+            throw new NotImplementedException();
         }
     }
 }
