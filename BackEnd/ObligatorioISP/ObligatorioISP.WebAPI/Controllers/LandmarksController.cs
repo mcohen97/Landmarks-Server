fb780ecc2c9c1acdd2cfae8320e9e334c9b63334
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-18 22:02:33 -0300

    se agrega proyecto WebAPI

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.WebAPI/Controllers/LandmarksController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ObligatorioISP.DataAccess.Contracts;
+
+namespace ObligatorioISP.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LandmarksController : ControllerBase
+    {
+        private ILandmarksRepository landmarks;
+        public LandmarksController(ILandmarksRepository landmarksRepo) {
+            landmarks = landmarksRepo;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
