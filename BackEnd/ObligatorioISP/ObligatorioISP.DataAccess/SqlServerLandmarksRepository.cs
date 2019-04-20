3a781fa51f28937f69c6ece2303d2fd27acf84e6
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-20 13:01:59 -0300

    se agrega funcionalidad para que pase la prueba

--- BackEnd/ObligatorioISP/ObligatorioISP.DataAccess/SqlServerLandmarksRepository.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess/SqlServerLandmarksRepository.cs
@@ -1,7 +1,10 @@
 ﻿
 
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
+using ObligatorioISP.BusinessLogic;
 using ObligatorioISP.DataAccess.Contracts;
 using ObligatorioISP.DataAccess.Contracts.Dtos;
 
@@ -9,8 +12,14 @@ namespace ObligatorioISP.DataAccess
 {
     public class SqlServerLandmarksRepository: ILandmarksRepository
     {
+        private static string IMAGES_TABLE = "LandmarkImages";
+        private static string AUDIOS_TABLE = "LandmarkAudios";
+        private static string SEPARATOR = "_";
+
         private string connectionString;
-        public SqlServerLandmarksRepository(string connString) {
+        private string imagesDirectory = "Images";
+        private string audiosDirectory = "Audios";
+        public SqlServerLandmarksRepository(string connString, string imagesPath,string audiosPath) {
             connectionString = connString;
         }
 
@@ -30,13 +39,76 @@ namespace ObligatorioISP.DataAccess
                     using (SqlDataReader reader =sqlCmd.ExecuteReader()) {
                         
                         while (reader.Read()) {
-                            result.Add(new LandmarkDto());
+                            LandmarkDto dto = BuildLandmark(reader);
+                            result.Add(dto);
                         }
                     }
                 }
             }
 
             return result;
+        }
+
+        private LandmarkDto BuildLandmark(SqlDataReader reader)
+        {
+            int id = Int32.Parse(reader["ID"].ToString());
+            string title = reader["TITLE"].ToString();
+            string description = reader["DESCRIPT"].ToString();
+            double lat = double.Parse(reader["LATITUDE"].ToString());
+            double lng = double.Parse(reader["LONGITUDE"].ToString());
+            ICollection<string> images = GetMediaResources(id, IMAGES_TABLE);
+            ICollection<string> audios = GetMediaResources(id, AUDIOS_TABLE);
+
+            //important to instantiate Landmark first, because it checks that database data is consistent.
+            Landmark landmark = new Landmark(id, title, lat, lng,description ,images, audios);
+
+            return ConvertToDto(landmark);
+        }
+
+        private ICollection<string> GetMediaResources(int landmarkId, string table)
+        {
+            string command = $"SELECT * FROM {table} WHERE LANDMARK_ID = {landmarkId} ORDER BY ID ASC";
+
+            ICollection<string> result = new List<string>();
+
+            using (SqlConnection client = new SqlConnection(connectionString))
+            {
+                client.Open();
+                using (SqlCommand sqlCmd = new SqlCommand(command, client))
+                {
+                    using (SqlDataReader reader = sqlCmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string resourceId = reader["ID"].ToString();
+                            string path;
+                            if (table.Equals(IMAGES_TABLE))
+                            {
+                                string extension = reader["EXTENSION"].ToString();
+                                path = $"{imagesDirectory}/{landmarkId}{SEPARATOR}{resourceId}.{extension}";
+                            }
+                            else {
+                                path = $"{audiosDirectory}/{landmarkId}{SEPARATOR}{resourceId}.mp3";
+                            }
+                            result.Add(path);
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        private LandmarkDto ConvertToDto(Landmark landmark)
+        {
+            return new LandmarkDto()
+            {
+                Id = landmark.Id,
+                Title = landmark.Title,
+                Latitude = landmark.Latitude,
+                Longitude = landmark.Longitude,
+                Description = landmark.Description,
+                ImageBase64 = landmark.Images.First()
+            };
         }
     }
 }
