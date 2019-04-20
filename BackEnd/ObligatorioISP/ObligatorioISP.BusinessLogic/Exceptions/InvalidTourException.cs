c7ccec37755af51e040651a9b8de31e0f301c012
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-20 13:57:21 -0300

    se crea la excepcion y la clase Tour referenciados en la prueba con la implementacion necesaria para que la prueba pase

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.BusinessLogic/Exceptions/InvalidTourException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioISP.BusinessLogic.Exceptions
+{
+    public class InvalidTourException: Exception
+    {
+        public InvalidTourException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ BackEnd/ObligatorioISP/ObligatorioISP.BusinessLogic/Tour.cs
@@ -0,0 +1,28 @@
+﻿using ObligatorioISP.BusinessLogic.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioISP.BusinessLogic
+{
+    public class Tour
+    {
+        private int id;
+        private string title;
+        private ICollection<Landmark> landmarks;
+
+        public int Id { get { return id; }set { SetId(value); } }
+
+        public Tour(int anId, string aTitle, ICollection<Landmark> someLandmarks) {
+            Id = anId;
+        }
+
+        private void SetId(int value)
+        {
+            if (value < 0) {
+                throw new InvalidTourException("Id can't be negative");
+            }
+            id = value;
+        }
+    }
+}
