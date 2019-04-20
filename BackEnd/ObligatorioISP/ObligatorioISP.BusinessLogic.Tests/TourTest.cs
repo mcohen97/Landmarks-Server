f70e229d8f3a5e01e2150bea4ed8f43622372098
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-20 13:57:41 -0300

    se modifica la prueba de tour

--- BackEnd/ObligatorioISP/ObligatorioISP.BusinessLogic.Tests/TourTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.BusinessLogic.Tests/TourTest.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Moq;
+using ObligatorioISP.BusinessLogic.Exceptions;
+using System.IO;
 
 namespace ObligatorioISP.BusinessLogic.Tests
 {
@@ -19,15 +21,22 @@ namespace ObligatorioISP.BusinessLogic.Tests
         [TestMethod]
         [ExpectedException(typeof(InvalidTourException))]
         public void ShouldThrowExceptionWhenIdIsNegative() {
-            Tour testTour = new Tour(1, "Tour 1", fakeLandmarks);
+            Tour testTour = new Tour(-1, "Tour 1", fakeLandmarks);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidTourException))]
+        public void ShouldThrowExceptionWhenTitleEmpty()
+        {
+            Tour testTour = new Tour(-1, " ", fakeLandmarks);
         }
 
         private ICollection<Landmark> GetFakeLandmarks()
         {
-            //(string aTitle, double lat, double lng, string aDescription, string aPath)
-            Mock<Landmark> fake1 = new Mock<Landmark>("Landmark 1", -34.912126, -56.167282, "Description 1", "image 1");
-            Mock<Landmark> fake2 = new Mock<Landmark>("Landmark 2", -34.912127, -56.167281, "Description 2", "image 2");
-            Mock<Landmark> fake3 = new Mock<Landmark>("Landmark 3", -34.912128, -56.167284, "Description 3", "image 3");
+            File.Create("1_1.jpg");
+            Mock<Landmark> fake1 = new Mock<Landmark>("Landmark 1", -34.912126, -56.167282, "Description 1", "1_1.jpg");
+            Mock<Landmark> fake2 = new Mock<Landmark>("Landmark 2", -34.912127, -56.167281, "Description 2", "1_1.jpg");
+            Mock<Landmark> fake3 = new Mock<Landmark>("Landmark 3", -34.912128, -56.167284, "Description 3", "1_1.jpg");
             return new List<Landmark>() { fake1.Object, fake2.Object, fake3.Object };
         }
     }
