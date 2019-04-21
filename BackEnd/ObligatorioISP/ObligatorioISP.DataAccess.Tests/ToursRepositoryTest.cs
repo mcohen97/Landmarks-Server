491a294fce23ce8cf42cafbe6909a579d4c711a4
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-21 16:41:52 -0300

    se crea clase de prueba para el repositorio de tours que se creara

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Tests/ToursRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioISP.DataAccess.Tests
+{
+    [TestClass]
+    public class ToursRepositoryTest
+    {
+        [TestInitialize]
+        public void SetUp() {
+
+        }
+    }
+}
