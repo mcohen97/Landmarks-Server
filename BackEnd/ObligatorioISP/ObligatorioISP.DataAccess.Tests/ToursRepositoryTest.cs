54209e3f46042ba5f0ac847d2acf20c153ccad3a
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-21 18:09:28 -0300

    se escriben pruebas para ToursRepository

--- BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Tests/ToursRepositoryTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Tests/ToursRepositoryTest.cs
@@ -8,9 +8,22 @@ namespace ObligatorioISP.DataAccess.Tests
     [TestClass]
     public class ToursRepositoryTest
     {
+        private SqlServerLandmarksRepository landmarks;
+        private SqlServerToursRepository tours;
+        private TestDatabaseManager testData;
+
         [TestInitialize]
         public void SetUp() {
+            testData = new TestDatabaseManager();
+            testData.SetUpDatabase();
+            testData.LoadTestData();
+            tours = new SqlServerToursRepositorys(testData.ConnectionString,landmarks);
+        }
 
+        [TestMethod]
+        public void ShouldReturnTourGivenExistingId() {
+            LandmarkDto retrieved = tours.GetById(1);
+            Assert.AreEqual(1, retrieved.Id);
         }
     }
 }
