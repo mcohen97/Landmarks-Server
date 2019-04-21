ca413bc5d5c702fd65f228c20f2fdd2296805b1d
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-21 18:06:14 -0300

    se abstrae la logica de creacion de base de datos de prueba a una clase aparte para ser reutilizada

--- BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Tests/LandmarksRepositoryTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Tests/LandmarksRepositoryTest.cs
@@ -15,18 +15,16 @@ namespace ObligatorioISP.DataAccess.Tests
     public class LandmarksRepositoryTest
     {
         private SqlServerLandmarksRepository landmarks;
-        private string serverString = $"Server=DESKTOP-JH1M2MF\\SQLSERVER_R14;";
-        private string securityString = "Trusted_Connection=True;Integrated Security=True;";
-        private string dbName = "LandmarksTestDB";
-        private string imagesPath = "Images";
-        private string audiosPath = "Audios";
+        private TestDatabaseManager testData;
+
 
         [TestInitialize]
         public void StartUp()
         {
-            string connString = serverString + $"Initial Catalog={dbName};" + securityString;
-            SetUpDatabase(connString);
-            landmarks = new SqlServerLandmarksRepository(connString,imagesPath,audiosPath);
+            testData = new TestDatabaseManager();
+            testData.SetUpDatabase();
+            testData.LoadTestData();
+            landmarks = new SqlServerLandmarksRepository(testData.ConnectionString,testData.ImagesPath,testData.AudiosPath);
         }
 
         [TestMethod]
@@ -39,95 +37,6 @@ namespace ObligatorioISP.DataAccess.Tests
 
             ICollection<LandmarkDto> withinBounds = landmarks.GetWithinCoordenates(bottomLeftLat, bottomLeftLng, topRightLat, topRightLng);
             Assert.AreEqual(3, withinBounds.Count);
-        }
-
-        private void SetUpDatabase(string finalConnString)
-        {
-
-            if (!DbExists(dbName))
-            {
-                CreateDB(dbName);
-            }
-            LoadDatabase(finalConnString);
-            CreateImageFiles(finalConnString,imagesPath);
-        }
-
-        private bool DbExists(string database)
-        {
-            string connString = serverString + "Initial Catalog=master;" + securityString;
-            string cmdText = "SELECT * FROM master.dbo.sysdatabases WHERE NAME=\'" + database + "\'";
-            bool exists = false;
-            using (SqlConnection client = new SqlConnection(connString))
-            {
-                client.Open();
-
-                using (SqlCommand sqlCmd = new SqlCommand(cmdText, client))
-                {
-
-                    SqlDataReader reader = sqlCmd.ExecuteReader();
-
-                    exists = reader.HasRows;
-
-                    reader.Close();
-
-                }
-            }
-            return exists;
-        }
-
-        private void CreateDB(string connectionString)
-        {
-            using (SqlConnection client = new SqlConnection(connectionString))
-            {
-                client.Open();
-                string cmd = $"CREATE DATABASE {dbName} ; ";
-                using (SqlCommand sqlCmd = new SqlCommand(cmd, client))
-                {
-                    sqlCmd.ExecuteNonQuery();
-                }
-               
-            }
-        }
-
-        private void LoadDatabase(string connectionString)
-        {
-            using (SqlConnection client = new SqlConnection(connectionString))
-            {
-                client.Open();
-                string createScript = File.ReadAllText(@"..\..\..\..\Database\create_tables.sql");
-                using (SqlCommand sqlCmd = new SqlCommand(createScript, client))
-                {
-                    sqlCmd.ExecuteNonQuery();
-                }
-                string testDataScript = File.ReadAllText(@"..\..\..\..\Database\create_test_data.sql");
-                using (SqlCommand sqlCmd = new SqlCommand(testDataScript, client))
-                {
-                    sqlCmd.ExecuteNonQuery();
-                }
-            }
-        }
-
-        private void CreateImageFiles(string connectionString,string imagesPath)
-        {
-            using (SqlConnection client = new SqlConnection(connectionString))
-            {
-                client.Open();
-                string getIdsScript = "SELECT * FROM LandmarkImages;";
-                using (SqlCommand sqlCmd = new SqlCommand(getIdsScript, client))
-                {
-                    using (SqlDataReader reader = sqlCmd.ExecuteReader())
-                    {
-
-                        while (reader.Read())
-                        {
-                            string landmarkId = reader["LANDMARK_ID"].ToString();
-                            string id = reader["ID"].ToString();
-                            string extension = reader["EXTENSION"].ToString();
-                            File.Create($"{imagesPath}/{landmarkId}_{id}.{extension}");
-                        }
-                    }
-                }
-            }
         }
     }
 }
---
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Tests/TestDatabaseManager.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+
+namespace ObligatorioISP.DataAccess.Tests
+{
+    public class TestDatabaseManager
+    {
+        private string serverString = $"Server=DESKTOP-JH1M2MF\\SQLSERVER_R14;";
+        private string securityString = "Trusted_Connection=True;Integrated Security=True;";
+        public string dbName = "LandmarksTestDB";
+        public string ImagesPath { get; private set; }
+        public string AudiosPath { get; private set; }
+        public string ConnectionString { get; private set; }
+
+        public TestDatabaseManager() {
+            ConnectionString = serverString + $"Initial Catalog={dbName};" + securityString;
+            ImagesPath = "Images";
+            AudiosPath = "Audios";
+        }
+
+        public void SetUpDatabase()
+        {
+            string finalConnString = ConnectionString;
+
+            if (!DbExists(dbName))
+            {
+                CreateDB(dbName);
+            }
+        }
+
+        private bool DbExists(string database)
+        {
+            string connString = serverString + "Initial Catalog=master;" + securityString;
+            string cmdText = "SELECT * FROM master.dbo.sysdatabases WHERE NAME=\'" + database + "\'";
+            bool exists = false;
+            using (SqlConnection client = new SqlConnection(connString))
+            {
+                client.Open();
+
+                using (SqlCommand sqlCmd = new SqlCommand(cmdText, client))
+                {
+
+                    SqlDataReader reader = sqlCmd.ExecuteReader();
+
+                    exists = reader.HasRows;
+
+                    reader.Close();
+
+                }
+            }
+            return exists;
+        }
+
+        private void CreateDB(string connectionString)
+        {
+            using (SqlConnection client = new SqlConnection(connectionString))
+            {
+                client.Open();
+                string cmd = $"CREATE DATABASE {dbName} ; ";
+                using (SqlCommand sqlCmd = new SqlCommand(cmd, client))
+                {
+                    sqlCmd.ExecuteNonQuery();
+                }
+
+            }
+        }
+
+        public void LoadTestData()
+        {
+            using (SqlConnection client = new SqlConnection(ConnectionString))
+            {
+                client.Open();
+                ResetDatabase(client);
+                string testDataScript = File.ReadAllText(@"..\..\..\..\Database\create_test_data.sql");
+                using (SqlCommand sqlCmd = new SqlCommand(testDataScript, client))
+                {
+                    sqlCmd.ExecuteNonQuery();
+                }
+            }
+            CreateImageFiles(ConnectionString, ImagesPath);
+        }
+
+        private void ResetDatabase(SqlConnection client)
+        {
+            client.Open();
+            string createScript = File.ReadAllText(@"..\..\..\..\Database\create_tables.sql");
+            using (SqlCommand sqlCmd = new SqlCommand(createScript, client))
+            {
+                sqlCmd.ExecuteNonQuery();
+            }
+        }
+
+        private void CreateImageFiles(string connectionString, string imagesPath)
+        {
+            using (SqlConnection client = new SqlConnection(connectionString))
+            {
+                client.Open();
+                string getIdsScript = "SELECT * FROM LandmarkImages;";
+                using (SqlCommand sqlCmd = new SqlCommand(getIdsScript, client))
+                {
+                    using (SqlDataReader reader = sqlCmd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            string landmarkId = reader["LANDMARK_ID"].ToString();
+                            string id = reader["ID"].ToString();
+                            string extension = reader["EXTENSION"].ToString();
+                            File.Create($"{imagesPath}/{landmarkId}_{id}.{extension}");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
