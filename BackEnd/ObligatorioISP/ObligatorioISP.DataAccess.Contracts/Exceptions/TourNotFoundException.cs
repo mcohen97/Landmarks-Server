4d84f8589d0738661d4f11b45acabcfdd3f6b4cc
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-25 20:17:03 -0300

    se crea la excepcion referenciada en la prueba

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Contracts/Exceptions/TourNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ObligatorioISP.DataAccess.Contracts.Exceptions
+{
+    public class TourNotFoundException: Exception
+    {
+        public TourNotFoundException(string message) : base(message) {
+        }
+    }
+}
