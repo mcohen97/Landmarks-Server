c1787de1936c93196d257aba55f3267ab5e50999
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-25 17:53:21 -0300

    se crea clase y se implementa metodo

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.WebAPI/Controllers/ToursController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ObligatorioISP.DataAccess.Contracts;
+using ObligatorioISP.DataAccess.Contracts.Dtos;
+
+namespace ObligatorioISP.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToursController : ControllerBase
+    {
+        private IToursRepository tours;
+        public ToursController(IToursRepository toursRepo)
+        {
+            tours = toursRepo;
+        }
+
+        [HttpGet]
+        public IActionResult Get(double leftBottomLat, double leftBottomLng, double distanceInKm)
+        {
+            ICollection<TourDto> retrieved = tours.GetToursWithinKmRange(leftBottomLat, leftBottomLng, distanceInKm);
+            return Ok(retrieved);
+        }
+    }
+}
