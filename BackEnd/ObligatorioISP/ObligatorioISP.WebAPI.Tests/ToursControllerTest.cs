9410f3057b3d0857a8c5eef5f0c50148a8c2ffdd
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-25 17:52:49 -0300

    se escribe prueba para metodo de ToursController

--- BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/ToursControllerTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/ToursControllerTest.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using ObligatorioISP.DataAccess.Contracts;
+using ObligatorioISP.DataAccess.Contracts.Dtos;
+using ObligatorioISP.WebAPI.Controllers;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ObligatorioISP.WebAPI.Tests
@@ -11,12 +15,48 @@ namespace ObligatorioISP.WebAPI.Tests
     public class ToursControllerTest
     {
         private ToursController controller;
-        private Mock<IToursRepository> tours;
+        private Mock<IToursRepository> fakeToursStorage;
 
         [TestInitialize]
-        public void StartUp() {
-            tours = new Mock<IToursRepository>();
-            controller = new ToursController(tours.Object);
+        public void StartUp()
+        {
+            fakeToursStorage = new Mock<IToursRepository>();
+            fakeToursStorage.Setup(r => r.GetById(It.IsAny<int>())).Returns((int x) => GetFakeTours().FirstOrDefault(t => t.Id == x));
+            fakeToursStorage.Setup(r => r.GetToursWithinKmRange(It.IsAny<double>(), It.IsAny<double>(), It.IsAny<double>())).Returns(GetFakeTours());
+            controller = new ToursController(fakeToursStorage.Object);
+        }
+
+        [TestMethod]
+        public void ShouldReturnToursFromStorage()
+        {
+            double centerLat = -34.912127;
+            double centerLng = -56.167283;
+            double distance = 2;
+
+            IActionResult result = controller.Get(centerLat, centerLng, distance);
+            OkObjectResult ok = result as OkObjectResult;
+            List<TourDto> tours = ok.Value as List<TourDto>;
+
+            fakeToursStorage.Verify(r => r.GetToursWithinKmRange(centerLat, centerLng, distance));
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(ok);
+            Assert.AreEqual(200, ok.StatusCode);
+            Assert.IsNotNull(tours);
+            Assert.AreEqual(GetFakeTours().Count, tours.Count);
+        }
+
+        private ICollection<TourDto> GetFakeTours()
+        {
+            return new List<TourDto>() {
+                new TourDto(){
+                Id=1},
+                new TourDto(){
+                    Id=2
+                },
+                new TourDto(){
+                    Id = 3
+                }
+            };
         }
     }
 }
