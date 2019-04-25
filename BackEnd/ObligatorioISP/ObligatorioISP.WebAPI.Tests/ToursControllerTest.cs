ee4daf25caeeef51f44d3dfedea04eab8ae3b21d
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-25 16:42:01 -0300

    se crea clase de prueba para el controller de Tours

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/ToursControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ObligatorioISP.DataAccess.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioISP.WebAPI.Tests
+{
+    [TestClass]
+    public class ToursControllerTest
+    {
+        private ToursController controller;
+        private Mock<IToursRepository> tours;
+
+        [TestInitialize]
+        public void StartUp() {
+            tours = new Mock<IToursRepository>();
+            controller = new ToursController(tours.Object);
+        }
+    }
+}
