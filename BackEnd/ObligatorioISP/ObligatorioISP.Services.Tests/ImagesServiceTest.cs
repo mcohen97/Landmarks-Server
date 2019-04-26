5747eee68cba0f35f538fa96705f89f9357ae4ae
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-25 22:29:08 -0300

    se crea modulo de pruebas para una capa de servicios que se agregara

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.Services.Tests/ImagesServiceTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ObligatorioISP.Services.Tests
+{
+    [TestClass]
+    public class ImageServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
