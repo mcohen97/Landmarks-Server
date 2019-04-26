353f0adf7aa8340975e0bf5f6d2cfc565e75a9ff
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-25 23:16:09 -0300

    se crea modulo de implementaciones de servicios con el servicio de imagenes, y la funcion descrita por las pruebas implementada

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.Services/DiskImagesService.cs
@@ -0,0 +1,42 @@
+﻿using ObligatorioISP.Services.Contracts;
+using System;
+using System.IO;
+
+namespace ObligatorioISP.Services
+{
+    public class DiskImagesService : IImagesService
+    {
+
+        private string directory;
+        public DiskImagesService(string imagesFolder)
+        {
+            directory = imagesFolder;
+        }
+
+        public string GetImageInBase64(string imageName)
+        {
+            byte[] data;
+            try
+            {
+                data = TryRead(imageName);
+            }
+            catch (IOException)
+            {
+                data = new byte[0];
+            }
+            return Convert.ToBase64String(data);
+        }
+
+        private byte[] TryRead(string path)
+        {
+            byte[] bytes = new byte[0];
+            string fullPath = directory + "/" + path;
+            using (Stream source = File.OpenRead(fullPath))
+            {
+                bytes = new byte[source.Length];
+                source.Read(bytes, 0, bytes.Length);
+            }
+            return bytes;
+        }
+    }
+}
