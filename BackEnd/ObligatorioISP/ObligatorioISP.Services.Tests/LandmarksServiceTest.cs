e1c05acc8e36089610407ee8ea16f726b300bcbf
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-26 16:38:31 -0300

    se escriben pruebas para el servicio

--- BackEnd/ObligatorioISP/ObligatorioISP.Services.Tests/LandmarksServiceTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.Services.Tests/LandmarksServiceTest.cs
@@ -2,6 +2,7 @@
 using Moq;
 using ObligatorioISP.DataAccess.Contracts;
 using ObligatorioISP.DataAccess.Contracts.Dtos;
+using ObligatorioISP.Services.Contracts;
 using System.Collections.Generic;
 
 namespace ObligatorioISP.Services.Tests
@@ -17,6 +18,25 @@ namespace ObligatorioISP.Services.Tests
             landmarks.Setup(r => r.GetTourLandmarks(It.IsAny<int>())).Returns(GetFakeLandmarks());
             landmarks.Setup(r => r.GetWithinZone(It.IsAny<double>(), It.IsAny<double>(), It.IsAny<double>())).Returns(GetFakeLandmarks());
             service = new LandmarksService(landmarks.Object);
+        }
+
+        [TestMethod]
+        public void ShouldReturnLandmarksFromRepository() {
+            double lat = -34.923844;
+            double lng = -56.170590;
+            double dist = 2;
+
+            ICollection<LandmarkDto> retrieved = service.GetLandmarksWithinZone(lat, lng, dist);
+            landmarks.Verify(l => l.GetWithinZone(lat, lng, dist), Times.Once);
+            Assert.AreEqual(GetFakeLandmarks().Count, retrieved.Count);
+        }
+
+        [TestMethod]
+        public void ShouldReturnTourLandmarksFromRepository() {
+            int id = 1;
+            ICollection<LandmarkDto> retrieved = service.GetLandmarksOfTour(id);
+            landmarks.Verify(l => l.GetTourLandmarks(id),Times.Once);
+            Assert.AreEqual(GetFakeLandmarks().Count, retrieved.Count);
         }
 
         private ICollection<LandmarkDto> GetFakeLandmarks()
