f2e3eb1401481f710fa983bc5fc9378f6211130e
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-26 16:36:50 -0300

    se crea contrato del servicio e implementacion correspondiente

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.Services/LandmarksService.cs
@@ -0,0 +1,17 @@
+﻿using ObligatorioISP.DataAccess.Contracts;
+using ObligatorioISP.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioISP.Services
+{
+    public class LandmarksService:ILandmarksService
+    {
+        private ILandmarksRepository landmarks;
+
+        public LandmarksService(ILandmarksRepository landmarksStorage) {
+            landmarks = landmarksStorage;
+        }
+    }
+}
