2c571d26ff65d7a9e03190c78b6fbca0e6ab24ea
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-25 22:51:59 -0300

    se escriben pruebas para servicio de imagenes

--- BackEnd/ObligatorioISP/ObligatorioISP.Services.Tests/ImagesServiceTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.Services.Tests/ImagesServiceTest.cs
@@ -1,13 +1,41 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
 
 namespace ObligatorioISP.Services.Tests
 {
     [TestClass]
-    public class ImageServiceTest
+    public class ImagesServiceTest
     {
+
+        private string imagesDirectory;
+        private string testImageName;
+        private string pixelImageBase64;
+
+        private IImagesService service;
+
+        [TestInitialize]
+        public void StartUp() {
+            testImageName = "testImage.jpg";
+            imagesDirectory = "Images";
+            pixelImageBase64 = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==";
+            service = new DiskImageService(imagesDirectory);
+            WriteTestImage();
+        }
+
         [TestMethod]
-        public void TestMethod1()
-        {
+        public void ShouldGetExistentImage() {
+
+        }
+
+        private void WriteTestImage() {
+            byte[] bytes = Convert.FromBase64String(pixelImageBase64);
+            string imagePath = imagesDirectory + "/" + testImageName;
+            using (var imageFile = new FileStream(imagePath, FileMode.Create))
+            {
+                imageFile.Write(bytes, 0, bytes.Length);
+                imageFile.Flush();
+            }
         }
     }
 }
