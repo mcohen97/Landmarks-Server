aba5f4f61b6b7cef8cb39281c2ae2f2192ca89cc
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-29 17:21:35 -0300

    se escriben pruebas para cubrir casos donde la conexion no funciona

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Tests/SqlServerConnectionManagerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioISP.DataAccess.Tests
+{
+    [TestClass]
+    class SqlServerConnectionManagerTest
+    {
+        private SqlServerConnectionManager connection;
+        [TestInitialize]
+        public void SetUp() {
+          string unexistentServer = "DESKTOP-JH1M2MF\\SQLSERVER_R15;Initial Catalog=DB;Trusted_Connection=True;Integrated Security=True;";
+          connection = new SqlServerConnectionManager(unexistentServer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataInaccessibleException))]
+        public void ShouldThrowExceptionIfCantReadData() {
+            string query = "query";
+            connection.ExcecuteRead(query);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DataInaccessibleException))]
+        public void ShouldThrowExceptionIfCantExcecuteCommmand() {
+            string command = "command";
+            connection.ExcecuteCommand(command);
+        }
+    }
+}
