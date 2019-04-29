b6d5be477da9f0e5ed91ad6b32479697d792e75a
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-29 20:16:23 -0300

    se escriben pruebas para testear que el sistema controle la consistencia de datos

--- BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Tests/LandmarksRepositoryTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Tests/LandmarksRepositoryTest.cs
@@ -3,6 +3,7 @@ using ObligatorioISP.BusinessLogic;
 using ObligatorioISP.DataAccess.Contracts.Exceptions;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using Moq;
 
 namespace ObligatorioISP.DataAccess.Tests
 {
@@ -12,7 +13,7 @@ namespace ObligatorioISP.DataAccess.Tests
     {
         private SqlServerLandmarksRepository landmarks;
         private TestDatabaseManager testData;
-
+        private ISqlContext context;
 
         [TestInitialize]
         public void StartUp()
@@ -20,7 +21,8 @@ namespace ObligatorioISP.DataAccess.Tests
             testData = new TestDatabaseManager();
             testData.SetUpDatabase();
             testData.LoadTestData();
-            landmarks = new SqlServerLandmarksRepository(testData.ConnectionString,testData.ImagesPath,testData.AudiosPath);
+            context = new SqlServerConnectionManager(testData.ConnectionString);
+            landmarks = new SqlServerLandmarksRepository(context,testData.ImagesPath,testData.AudiosPath);
         }
 
         [TestMethod]
@@ -52,6 +54,23 @@ namespace ObligatorioISP.DataAccess.Tests
         [ExpectedException(typeof(LandmarkNotFoundException))]
         public void ShouldThrowExceptionIfLandmarkNotFound() {
             Landmark retrieved = landmarks.GetById(32);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CorruptedDataException))]
+        public void ShouldThrowExceptionWhenDatabaseHasInconsistencies() {
+            Mock<ISqlContext> fakeContext = new Mock<ISqlContext>();
+            Dictionary<string, object> faultyLandmarkData = new Dictionary<string, object>();
+            faultyLandmarkData.Add("ID", -1);
+            faultyLandmarkData.Add("TITLE", "");
+            faultyLandmarkData.Add("LATITUDE", -34.923844);
+            faultyLandmarkData.Add("LONGITUDE", -56.170590);
+            faultyLandmarkData.Add("DESCRIPT", "");
+            faultyLandmarkData.Add("EXTENSION", "");
+            ICollection<Dictionary<string, object>> fakeReturn = new List<Dictionary<string, object>>() { faultyLandmarkData };
+            fakeContext.Setup(c => c.ExcecuteRead(It.IsAny<string>())).Returns(fakeReturn);
+            landmarks = new SqlServerLandmarksRepository(fakeContext.Object, testData.ImagesPath, testData.AudiosPath);
+            landmarks.GetById(2);
         }
     }
 }
