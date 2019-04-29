4e9435464c3b509df7b4805041d77785ec802929
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-29 20:52:18 -0300

    se crea clase de prueba para repositorio de audios

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess.Tests/AudiosRepositoryTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioISP.DataAccess.Tests
+{
+    [TestClass]
+    public class AudiosRepositoryTest
+    {
+
+        private IAudiosRepository audios;
+        private string audiosDirectory;
+
+        [TestInitialize]
+        public void StartUp() {
+            audios = new DiskAudiosRepository();
+        }
+    }
+}
