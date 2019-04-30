42bb15ca71bf64ab96509e031b5ae457b815f252
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-29 21:36:51 -0300

    se crea el contrato del repositorio y una implementacion

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.DataAccess/DiskAudiosRepository.cs
@@ -0,0 +1,35 @@
+﻿using ObligatorioISP.DataAccess.Contracts;
+using System;
+using System.IO;
+
+namespace ObligatorioISP.DataAccess
+{
+    public class DiskAudiosRepository : IAudiosRepository
+    {
+        public string GetAudioInBase64(string audioPath)
+        {
+            byte[] data;
+            try
+            {
+                data = TryRead(audioPath);
+            }
+            catch (IOException)
+            {
+                data = new byte[0];
+            }
+            return Convert.ToBase64String(data);
+        }
+
+        private byte[] TryRead(string path)
+        {
+            byte[] bytes = new byte[0];
+            using (Stream source = File.OpenRead(path))
+            {
+                bytes = new byte[source.Length];
+                source.Read(bytes, 0, bytes.Length);
+            }
+            return bytes;
+        }
+
+    }
+}
