a5278b6a60f9fd83ad4debac12fd85d94c55ba8f
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-29 21:37:13 -0300

    se agregan pruebas al servicio para corroborar que se integre correctamente con este nuevo repositorio

--- BackEnd/ObligatorioISP/ObligatorioISP.Services.Tests/LandmarksServiceTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.Services.Tests/LandmarksServiceTest.cs
@@ -15,18 +15,24 @@ namespace ObligatorioISP.Services.Tests
         private ILandmarksService service;
         private Mock<ILandmarksRepository> landmarks;
         private Mock<IImagesRepository> images;
+        private Mock<IAudiosRepository> audios;
+
         private string testImageData;
+        private string testAudioData;
 
         [TestInitialize]
         public void SetUp() {
             SetUpRepositories();
-            service = new LandmarksService(landmarks.Object, images.Object);
+            service = new LandmarksService(landmarks.Object, images.Object, audios.Object);
         }
 
         private void SetUpRepositories() {
-            testImageData = "data";
+            testImageData = "imageData";
+            testAudioData = "audioData";
             images = new Mock<IImagesRepository>();
             images.Setup(i => i.GetImageInBase64(It.IsAny<string>())).Returns(testImageData);
+            audios = new Mock<IAudiosRepository>();
+            audios.Setup(a => a.GetAudioInBase64(It.IsAny<string>())).Returns(testAudioData);
             landmarks = new Mock<ILandmarksRepository>();
             landmarks.Setup(r => r.GetTourLandmarks(It.IsAny<int>())).Returns(GetFakeLandmarks());
             landmarks.Setup(r => r.GetWithinZone(It.IsAny<double>(), It.IsAny<double>(), It.IsAny<double>())).Returns(GetFakeLandmarks());
@@ -41,6 +47,7 @@ namespace ObligatorioISP.Services.Tests
             ICollection<LandmarkDto> retrieved = service.GetLandmarksWithinZone(lat, lng, dist);
             landmarks.Verify(l => l.GetWithinZone(lat, lng, dist), Times.Once);
             images.Verify(i => i.GetImageInBase64(It.IsAny<string>()), Times.Exactly(retrieved.Count));
+            audios.Verify(a => a.GetAudioInBase64(It.IsAny<string>()), Times.Exactly(2));
             Assert.AreEqual(GetFakeLandmarks().Count, retrieved.Count);
         }
 
@@ -59,11 +66,19 @@ namespace ObligatorioISP.Services.Tests
             if (!File.Exists(testImage)) {
                 File.Create(testImage);
             }
+            string testAudio = "testAudio.mp3";
+            if (!File.Exists(testAudio)) {
+                File.Create(testAudio);
+            }
+
+            ICollection<string> testAudios = new List<string>() { testAudio };
+            ICollection<string> testImages = new List<string>() { testImage };
+
             ICollection<Landmark> sampleList = new List<Landmark>() {
                 new Landmark(1, "Landmark 1",-34.912126,-56.167282,"Description 1", testImage),
-                new Landmark(2,"Landmark 2",-34.912900,-56.162263,"Description 2",testImage),
+                new Landmark(2,"Landmark 2",-34.912900,-56.162263,"Description 2",testImages,testAudios),
                 new Landmark(3,"Landmark 3",-34.914202,-56.157930,"Description 3",testImage),
-                new Landmark(4,"Landmark 4", -34.910866,-56.183353,"Description 4",testImage)
+                new Landmark(4,"Landmark 4", -34.910866,-56.183353,"Description 4",testImages,testAudios)
             };
             return sampleList;
         }
