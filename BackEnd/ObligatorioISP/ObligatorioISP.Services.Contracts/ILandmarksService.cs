94a5e648d1405f4cadf7e45d4bf88fa7e23b2a43
Marcel Cohen <diegomarcel27@hotmail.com>
2019-04-29 22:33:18 -0300

    se actualiza el contrato de servicios de landmarks

--- BackEnd/ObligatorioISP/ObligatorioISP.Services.Contracts/ILandmarksService.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.Services.Contracts/ILandmarksService.cs
@@ -7,7 +7,7 @@ namespace ObligatorioISP.Services.Contracts
 {
     public interface ILandmarksService
     {
-        ICollection<LandmarkDto> GetLandmarksWithinZone(double latitude, double longitude, double distance);
-        ICollection<LandmarkDto> GetLandmarksOfTour(int id);
+        ICollection<LandmarkSummarizedDto> GetLandmarksWithinZone(double latitude, double longitude, double distance);
+        ICollection<LandmarkSummarizedDto> GetLandmarksOfTour(int id);
     }
 }
