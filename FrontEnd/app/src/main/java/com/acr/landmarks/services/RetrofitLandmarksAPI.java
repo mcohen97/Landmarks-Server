20c912555d8f49de4d7a7dcfcca073eef3e58a0b
Marcel Cohen <diegomarcel27@hotmail.com>
2019-05-06 00:25:17 -0300

    se crea nueva llamada a API para obtener landmark por id, tambien se integra el servicio de landmarks con el de ubicacion

--- FrontEnd/app/src/main/java/com/acr/landmarks/services/RetrofitLandmarksAPI.java
+++ FrontEnd/app/src/main/java/com/acr/landmarks/services/RetrofitLandmarksAPI.java
@@ -6,11 +6,14 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface RetrofitLandmarksAPI {
 
     @GET("landmarks")
-    Call<List<Landmark>> getLandmarksInRange(@Query("lat") double latitude, @Query("lng") double longitude, @Query("dist") float distance);
+    Call<List<Landmark>> getLandmarksInRange(@Query("lat") double latitude, @Query("lng") double longitude, @Query("dist") double distance);
 
+    @GET("landmarks/{id}")
+    Call<Landmark> getLandmark(@Path("id") int id);
 }
--- FrontEnd/app/src/main/java/com/acr/landmarks/services/RetrofitLandmarksService.java
+++ FrontEnd/app/src/main/java/com/acr/landmarks/services/RetrofitLandmarksService.java
@@ -2,9 +2,11 @@ package com.acr.landmarks.services;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.location.Location;
 
 import com.acr.landmarks.models.Landmark;
 import com.acr.landmarks.services.contracts.ILandmarksService;
+import com.acr.landmarks.services.contracts.ILocationService;
 
 import java.util.List;
 
@@ -14,10 +16,15 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class RetrofitLandmarksService implements  ILandmarksService {
+public class RetrofitLandmarksService implements  ILandmarksService, ILocationService.OnGeofenceChangeListener {
 
     private Retrofit retrofit;
     private RetrofitLandmarksAPI webService;
+    private ILocationService locationService;
+    private Location currentLocation;
+    private double currentRadius;
+    private final MutableLiveData<List<Landmark>> data;
+
 
     public RetrofitLandmarksService(){
         retrofit = new Retrofit.Builder()
@@ -25,14 +32,17 @@ public class RetrofitLandmarksService implements  ILandmarksService {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         webService= retrofit.create(RetrofitLandmarksAPI.class);
+        locationService= LocationService.getInstance();
+        locationService.addGeofenceChangeListener(this);
+        currentLocation = locationService.getLocation();
+        currentRadius = locationService.getRadius();
+        data = new MutableLiveData<>();
     }
 
 
     @Override
-    public LiveData<List<Landmark>> getLandmarks(double latitude, double longitude, float distance) {
-         final MutableLiveData<List<Landmark>> data = new MutableLiveData<>();
-
-        Call<List<Landmark>> landmarks = webService.getLandmarksInRange(latitude,longitude,distance);
+    public LiveData<List<Landmark>> getLandmarks() {
+        Call<List<Landmark>> landmarks = webService.getLandmarksInRange(currentLocation.getLatitude(),currentLocation.getLongitude(),currentRadius);
         landmarks.enqueue(new Callback<List<Landmark>>() {
             @Override
             public void onResponse(Call<List<Landmark>> call, Response<List<Landmark>> response) {
@@ -47,5 +57,33 @@ public class RetrofitLandmarksService implements  ILandmarksService {
             }
         });
         return data;
+    }
+
+
+    @Override
+    public LiveData<Landmark> getLandmarkById(int id) {
+        final MutableLiveData<Landmark> data = new MutableLiveData<>();
+        Call<Landmark> landmark = webService.getLandmark(id);
+        landmark.enqueue(new Callback<Landmark>() {
+            @Override
+            public void onResponse(Call<Landmark> call, Response<Landmark> response) {
+                if(response.isSuccessful()){
+                    data.setValue(response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Landmark> call, Throwable t) {
+
+            }
+        });
+        return  data;
+    }
+
+    @Override
+    public void onGeofenceRadiusChange(Location newLocation, double newRadius) {
+        currentLocation=newLocation;
+        currentRadius= newRadius;
+        getLandmarks();
     }
 }
