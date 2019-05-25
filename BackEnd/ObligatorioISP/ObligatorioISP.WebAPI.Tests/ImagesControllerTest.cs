c13cde3d2526f8c6bcd3785f6c93c47c514ea982
Marcel Cohen <diegomarcel27@hotmail.com>
2019-05-25 19:36:15 -0300

    se crea clase de prueba para controller de imagenes

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/ImagesControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ObligatorioISP.WebAPI.Tests
+{
+    [TestClass]
+    public class ImagesControllerTest
+    {
+        private ImagesController controller;
+
+        [TestInitialize]
+        public void StartUp() {
+
+        }
+    }
+}
