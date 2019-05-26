08ac8253a502ac9505831253eadf54644c20d58f
Marcel Cohen <diegomarcel27@hotmail.com>
2019-05-25 21:21:34 -0300

    se crea controller que expone las imagenes accesibles por url

--- BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/ImagesControllerTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/ImagesControllerTest.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ObligatorioISP.WebAPI.Controllers;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ObligatorioISP.WebAPI.Tests
 {
@@ -9,10 +12,43 @@ namespace ObligatorioISP.WebAPI.Tests
     public class ImagesControllerTest
     {
         private ImagesController controller;
+        private Mock<IConfiguration> config;
+        private string configKey;
+        private string testImage;
 
         [TestInitialize]
         public void StartUp() {
+            configKey = "LandmarkImages:Uri";
+            config = new Mock<IConfiguration>();
+            config.Setup(c => c[configKey]).Returns(Directory.GetCurrentDirectory());
+            testImage = "testImage.jpg";
+            if (!File.Exists(testImage)) {
+                File.Create(testImage);
+            }
+            controller = new ImagesController(config.Object);
+        }
 
+        [TestMethod]
+        public void GetExistentImage() {
+            Task<IActionResult> task =controller.Get(testImage);
+            task.Wait();
+            IActionResult result = task.Result;
+            FileStreamResult stream = result as FileStreamResult;
+
+            Assert.IsNotNull(stream);
+            Assert.AreEqual("image/jpeg",stream.ContentType);
+        }
+
+        [TestMethod]
+        public void GerUnexistentImage() {
+            Task<IActionResult> task = controller.Get("unexistent.jpg");
+            task.Wait();
+
+            IActionResult result = task.Result;
+            ObjectResult error = result as ObjectResult;
+
+            Assert.IsNotNull(error);
+            Assert.AreEqual(404, error.StatusCode);
         }
     }
 }
---
+++ BackEnd/ObligatorioISP/ObligatorioISP.WebAPI/Controllers/ImagesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ObligatorioISP.Services.Contracts.Dtos;
+using ObligatorioISP.Services.Contracts.Exceptions;
+
+namespace ObligatorioISP.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ImagesController : Controller
+    {
+        private string imagesDirectory;
+
+        public ImagesController(IConfiguration configuration) {
+            imagesDirectory = configuration["LandmarkImages:Uri"];
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            IActionResult result;
+            try
+            {
+                string path = $"{imagesDirectory}/{id}";
+                FileStream image = System.IO.File.OpenRead(path);
+                result = File(image, "image/jpeg");
+            }
+            catch (System.IO.IOException) {
+                result = NotFound(new ErrorDto() { ErrorMessage = "Image not found" });
+            }
+            return result;
+        }
+    }
+}
