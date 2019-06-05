80419ecddb16413586a242612bb4a39c3371eda9
Marcel Cohen <diegomarcel27@hotmail.com>
2019-06-05 19:43:37 -0300

    se crea clase de prueba para servicio de notificaciones

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.Services.Tests/FirebaseNotificationServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioISP.Services.Tests
+{
+    [TestClass]
+
+    public class FirebaseNotificationServiceTest
+    {
+        [TestInitialize]
+        public void SetUp() {
+
+        }
+    }
+}
