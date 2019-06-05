1f88e4fbdace0db2238cdeec9c5c4d8a0dae5f65
Marcel Cohen <diegomarcel27@hotmail.com>
2019-06-05 20:46:21 -0300

    se crea contrato de servicio de notificaciones e clase de implementacion con Firebase

--- BackEnd/ObligatorioISP/ObligatorioISP.Services.Tests/FirebaseNotificationServiceTest.cs
+++ BackEnd/ObligatorioISP/ObligatorioISP.Services.Tests/FirebaseNotificationServiceTest.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Microsoft.Extensions.Configuration;
+using ObligatorioISP.Services.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ObligatorioISP.DataAccess.Contracts;
 
 namespace ObligatorioISP.Services.Tests
 {
@@ -9,9 +13,14 @@ namespace ObligatorioISP.Services.Tests
 
     public class FirebaseNotificationServiceTest
     {
+        private IProximityNotificationService testService;
+        private Mock<ILandmarksRepository> landmarks;
+
         [TestInitialize]
         public void SetUp() {
-
+            Mock<IConfiguration> config = new Mock<IConfiguration>();
+            landmarks = new Mock<ILandmarksRepository>();
+            testService = new FirebaseNotificationService(config.Object, landmarks.Object);
         }
     }
 }
---
+++ BackEnd/ObligatorioISP/ObligatorioISP.Services/FirebaseNotificationService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using ObligatorioISP.DataAccess.Contracts;
+using ObligatorioISP.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObligatorioISP.Services
+{
+    public class FirebaseNotificationService : IProximityNotificationService
+    {
+        private ILandmarksRepository landmarks;
+        private string applicationID;
+        private string senderID;
+
+        public FirebaseNotificationService(IConfiguration configuration, ILandmarksRepository landmarksStorage)
+        {
+            landmarks = landmarksStorage;
+            applicationID = configuration["Firebase:ApplicationID"];
+            senderID = configuration["Firebase:SenderID"];
+
+        }
+
+    }
+}
