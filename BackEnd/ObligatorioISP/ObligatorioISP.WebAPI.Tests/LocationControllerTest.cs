a843a3d8b55b89c55c4b713342525dd76ed7f75b
Marcel Cohen <diegomarcel27@hotmail.com>
2019-06-06 14:10:12 -0300

    se agrega clase de prueba para controller de notificaciones de cercania

---
+++ BackEnd/ObligatorioISP/ObligatorioISP.WebAPI.Tests/LocationControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ObligatorioISP.Services.Contracts;
+using ObligatorioISP.WebAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObligatorioISP.WebAPI.Tests
+{
+    [TestClass]
+    public class LocationControllerTest
+    {
+        private Mock<IProximityNotificationService> fakeService;
+        private LocationController controller;
+
+        [TestInitialize]
+        public void SetUp() {
+            fakeService = new Mock<IProximityNotificationService>();
+            controller = new LocationController(fakeService.Object);
+        }
+
+
+    }
+}
